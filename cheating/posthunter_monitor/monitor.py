f9a67c8ae3a66646252607c44b062b6f31d5eb2c
sdiki1 <volodymyrbuharin@yandex.ru>
2025-04-17 17:19:57 +0300

    fixed posthunter

--- cheating/posthunter_monitor/monitor.py
+++ cheating/posthunter_monitor/monitor.py
@@ -1,10 +1,69 @@
 import datetime, random, time
 from tg_bot.models import Task, Account, sessionmaker, services, engine, PostHunterRequest, TaskManager
 import vk_api
+
+
 class Monitor:
     def __init__(self):
         ...
-
+    def check_post(self, hunt, api, url):
+        if url[-1] == "/":
+            domain = url.split("/")[-2]
+        else:
+            domain = url.split("/")[-1]
+        ans = api.wall.get(domain=domain)
+        for i in ans["items"]:
+            date = i["date"]
+            date_post = datetime.datetime.fromtimestamp(date)
+            if date_post < hunt.created_at:
+                continue
+            is_comment=False
+            comments = api.wall.get_comments(owner_id=i["from_id"], post_id=i["id"])
+            for j in comments["items"]:
+                if hunt.keyword in j["text"]:
+                    is_comment = True
+                    self.is_changed = False
+            # print(date_post, hunt.created_at, is_comment, date_post >= hunt.created_at)
+            
+            if date_post >= hunt.created_at and is_comment:
+                is_changed = True
+                intervals = {
+                    'like': random.randint(20, 30),
+                    'comment': random.randint(60, 70),
+                    'repost': random.randint(120, 180)
+                }
+                quantity = 3
+                url = f"https://vk.com/wall{i['from_id']}_{i['id']}"
+                task_type = "comment"
+                quantity = hunt.comments
+                for i in range(quantity):
+                    interval = hunt.interval*i
+                    TaskManager.create_task(
+                        task_type=task_type,
+                        url=url,
+                        params={'comment_text':'comment_text'},
+                        interval=interval
+                    )
+                task_type = "like"
+                quantity = hunt.likes
+                for i in range(quantity):
+                    interval = hunt.interval*i
+                    TaskManager.create_task(
+                        task_type=task_type,
+                        url=url,
+                        params={'comment_text':'comment_text'},
+                        interval=interval
+                    )
+                task_type = "repost"
+                quantity = hunt.reposts
+                for i in range(quantity):
+                    interval = hunt.interval*i
+                    TaskManager.create_task(
+                        task_type=task_type,
+                        url=url,
+                        params={'comment_text':'comment_text'},
+                        interval=interval)
+        
     def run(self):
         while True:
             try:
@@ -16,68 +75,12 @@ class Monitor:
                 session = vk_api.VkApi(token=account.token)
                 session.http.headers['User-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0'
                 api = session.get_api()
-
+                self.is_changed = False
                 for hunt in Hunted:
                     is_changed = False
                     for url in hunt.group_url:
                         try:
-                            
-                            if url[-1] == "/":
-                                domain = url.split("/")[-2]
-                            else:
-                                domain = url.split("/")[-1]
-                            # hunt.created_at = datetime.datetime.now()
-                            ans = api.wall.get(domain=domain)
-                            for i in ans["items"]:
-                                date = i["date"]
-                                date_post = datetime.datetime.fromtimestamp(date)
-                                if date_post < hunt.created_at:
-                                    continue
-                                is_comment=False
-                                comments = api.wall.get_comments(owner_id=i["from_id"], post_id=i["id"])
-                                for j in comments["items"]:
-                                    if hunt.keyword in j["text"]:
-                                        is_comment = True
-                                print(date_post, hunt.created_at, is_comment, date_post >= hunt.created_at)
-                                
-                                if date_post >= hunt.created_at and is_comment:
-                                    is_changed = True
-                                    intervals = {
-                                        'like': random.randint(20, 30),
-                                        'comment': random.randint(60, 70),
-                                        'repost': random.randint(120, 180)
-                                    }
-                                    quantity = 3
-                                    url = f"https://vk.com/wall{i['from_id']}_{i['id']}"
-                                    task_type = "comment"
-                                    quantity = hunt.comments
-                                    for i in range(quantity):
-                                        interval = hunt.interval*i
-                                        TaskManager.create_task(
-                                            task_type=task_type,
-                                            url=url,
-                                            params={'comment_text':'comment_text'},
-                                            interval=interval
-                                        )
-                                    task_type = "like"
-                                    quantity = hunt.likes
-                                    for i in range(quantity):
-                                        interval = hunt.interval*i
-                                        TaskManager.create_task(
-                                            task_type=task_type,
-                                            url=url,
-                                            params={'comment_text':'comment_text'},
-                                            interval=interval
-                                        )
-                                    task_type = "repost"
-                                    quantity = hunt.reposts
-                                    for i in range(quantity):
-                                        interval = hunt.interval*i
-                                        TaskManager.create_task(
-                                            task_type=task_type,
-                                            url=url,
-                                            params={'comment_text':'comment_text'},
-                                            interval=interval)
+                            self.check_post(hunt, api, url)
                         except:
                             pass
                     if is_changed:
